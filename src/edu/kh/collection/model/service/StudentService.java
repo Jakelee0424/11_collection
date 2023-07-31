bcb6d2f50b15e1341e2c1dc2bc313b6a302ae8e5
jakelee0424 <qkql006@gmail.com>
2023-07-31 17:46:31 +0900

    done list

--- src/edu/kh/collection/model/service/StudentService.java
+++ src/edu/kh/collection/model/service/StudentService.java
@@ -108,214 +108,144 @@ public class StudentService {
 			}
 
 		}while(menuNum != 0);
-		
-		
 	}
 
-	/**
-	 *  학생 정보추가 메서드
-	 *  - 추가 성공시 "성공", 실패시 "실패" 문자열 반환
-	 */
 	public String addStudent() throws InputMismatchException {
-		System.out.println("========= 학생 정보 추가 =========");
+		System.out.println("======= 학생 정보 추가 =======");
 		
-		System.out.print("이름 >> ");
+		System.out.print("이름 입력 >> ");
 		String name = sc.next();
 		
-		System.out.print("나이 >> ");
+		System.out.print("나이 입력 >> ");
 		int age = sc.nextInt();
 		
-		sc.nextLine(); // 입력 버퍼 개행 문자 제거
-		System.out.print("거주지 >> ");
+		sc.nextLine();
+		System.out.print("거주지 입력 >> ");
 		String region = sc.nextLine();
 		
-		System.out.print("성별(M/F) >> ");
+		System.out.print("성별 입력 >> ");
 		char gender = sc.next().charAt(0);
 		
-		System.out.print("성적 >> ");
+		System.out.print("점수 입력 >> ");
 		int score = sc.nextInt();
 		
-		// student 객체 생성 후 List에 추가
-		
-		if(	studentList.add(new Student(name, age, region, gender, score)) ) {
-			// boolean java.util.List.add
-			return "\n성공!\n";
-		} else {
-			return "\n실패!\n";
-		}
-		
-	}
+		studentList.add(new Student(name, age, region, gender, score));
 
-	/**
-	 * 학생 정보 조회 전체 메서드
-	 */
+		return "추가 성공 ";
+	}
+	
 	public void selectAll() {
-		// List는 인덱스가 있음. (from 0)
-		// List에 저장된 데이터의 개수를 얻어오는 방법
-		// -> 배열명.length 대신 사용
-		// ** size  -> int List.size()
+		System.out.println("======= 학생 정보 조희 =======");
 		
-		// List가 비어있는지 확인하는 방법
-		// ** boolean List.isEmpty() : 비어있으면 true 반환
-		
-		System.out.println("========= 학생 전체 조회 =========");
-		//studentList가 비어있는 경우 "학생 정보가 없습니다" 조회
-		
-//		if(studentList.size() == 0 ) {}
-		if(studentList.isEmpty()) {
-			System.out.println("학생 정보가 없습니다.\n");
-		return;
-		}
-//		
-//		for(int i = 0 ; i < studentList.size() ; i++) {
-//			System.out.println(studentList.get(i));
-//		}
-		
-//		향상된 for 문 (== for each문)
-//		컬렉션, 배열의 모든 요소를 순차적으로 반복접근할 수 있는 for 문
-//		(0번 인덱스 부터, 마지막 인덱스까지 1씩 증가)
-		int index =0;
 		for(Student std : studentList) {
-			System.out.print((index++) + "번 인덱스 ");
 			System.out.println(std);
 		}
+		if(studentList.isEmpty()) {
+			System.out.println("학생 정보가 없습니다.");
+		}
 		System.out.println();
+		
 	}
-	
-	/**
-	 * 학생 정보 수정 메서드
-	 */
+
 	public String updateStudent() throws InputMismatchException{
-		// set 메서드 : List.set(index, Student e);
-		// List의 i번째 요소를 e로 변경
-		// 반환값이 Student == 변경 전 Student 객체가 담겨있다.
+		System.out.println("======= 학생 정보 수정 =======");
 		
-		System.out.println("========= 학생 정보 수정 =========");
-		
-		System.out.print("인덱스 번호 입력 >>");
+		System.out.print("수정을 원하는 학생의 인덱스 번호를 입력하세요 >>");
 		int input = sc.nextInt();
 		
-		// 1) 학생 정보가 studentList에 있는가
-		// 2) 입력된 수가 0보다 작은가
-		// 3) 문자열을 입력했다면 예외처리 throws
-		// 4) 입력받은 숫자가 studentList 범위 내 인덱스 번호인가
+		System.out.println(studentList.get(input) + "\n");
+		System.out.print("수정을 진행하시겠습니까?(y/n) >>");
+		char input2 = sc.next().toUpperCase().charAt(0);
 		
-		if(studentList.isEmpty()) {
-			return "입력된 학생 정보가 없습니다.";
-		} else if(input < 0) {
-			return "음수는 입력할 수 없습니다.";
-		} else if(input >= studentList.size()){
-			return "범위를 넘어선 값을 입력할 수 없습니다.";
-		} else {
-			System.out.print(input + "번째 인덱스에 저장된 학생 정보");		
-			System.out.println(studentList.get(input));		
-			
-			System.out.print("이름 >> ");
+		if (input2 == 'Y') {		
+			System.out.print("\n이름 입력 >> ");
 			String name = sc.next();
-			
-			System.out.print("나이 >> ");
+
+			System.out.print("나이 입력 >> ");
 			int age = sc.nextInt();
-			
-			sc.nextLine(); // 입력 버퍼 개행 문자 제거
-			System.out.print("거주지 >> ");
+
+			sc.nextLine();
+			System.out.print("거주지 입력 >> ");
 			String region = sc.nextLine();
-			
-			System.out.print("성별(M/F) >> ");
+
+			System.out.print("성별 입력 >> ");
 			char gender = sc.next().charAt(0);
-			
-			System.out.print("성적 >> ");
+
+			System.out.print("점수 입력 >> ");
 			int score = sc.nextInt();
-			
-			Student temp  = studentList.set(input, new Student(name, age, region, gender, score));
-				
-			return temp.getName() + "의 정보가 변경되었습니다.\n";		
-		}
+
+			studentList.set(input, new Student(name, age, region, gender, score));
 		
-	}
-
-	/**
-	 * 학생 정보 제거 메서드 
-	 */
-	public String removeStudent() throws InputMismatchException{
-	System.out.println("========= 학생 정보 제거 =========");
-	
-		// List.remove(int index)
-		// 리스트에서 index번째 요소를 제거
-		// 이때, 제거된 요소가 반환된다.
-		// 제거되면 다음 번째 인덱스 요소를 당겨옴 (비어있는 인덱스 없음)
-	
-		System.out.print("인덱스 번호 입력 >>");
-		int input = sc.nextInt();
-
-		if(studentList.isEmpty()) {
-			return "입력된 학생 정보가 없습니다.";
-		} else if(input < 0) {
-			return "음수는 입력할 수 없습니다.";
-		} else if(input >= studentList.size()){
-			return "범위를 넘어선 값을 입력할 수 없습니다.";
+			return "\n** 수정 완료! **\n";
 		} else {
-			System.out.println(input + "인덱스의 학생 정보 = " + studentList.get(input) );	
-			System.out.println("\n정말 삭제 하시겠습니까?(y / n)");
-			char input2 = sc.next().toUpperCase().charAt(0); // toUpperCase : 대문자로 변경
-			if(input2 == 'Y') {
-				
-			Student temp = studentList.remove(input);
-			return temp.getName() + "의 정보가 변경되었습니다.\n";
-		
-			}else {
-				return "취소";
-			}
-			
+			return "\n** 수정 실패! **\n";
 		}
-		
 	}
 
-	/**
-	 * 학생 정보 검색(일치) 메서드
-	 */
-	public void searchStudent() throws InputMismatchException{
-		System.out.println("========= 학생 정보 검색(일치) =========");
+	public String removeStudent() throws InputMismatchException{
+		System.out.println("======= 학생 정보 제거 =======");
+		
+		System.out.print("정보 제거를 원하는 학생의 인덱스 번호를 입력하세요 >> ");
+		int input = sc.nextInt();
+		
+		if (input < 0 || input >= studentList.size()) {
+			return "잘못 입력하셨습니다.";
+		}
+			
+		System.out.println(studentList.get(input) + "\n");
+		System.out.print("정보 제거를 진행하시겠습니까?(y/n) >>");
+		char input2 = sc.next().toUpperCase().charAt(0);
+		
+		
+		if (input2 == 'Y') {	
+			studentList.remove(input);
+			return "\n정보 제거 완료!\n";
+		} else {
+			return "\n정보 제거 실패!\n";
+			
+		}
+	
+	}
 
-		System.out.print("검색할 학생 이름을 입력하세요 >> ");
+	public void searchStudent() {
+		System.out.println("======= 학생 정보 조회(전체) =======");
+		
+		System.out.print("학생 이름 입력 >> ");
 		String input = sc.next();
-
-		boolean flag = true;
+		
+		boolean var = true;
 		
 		for(Student std : studentList) {
 			if(input.equals(std.getName())) {
 				System.out.println(std + "\n");
-				flag = false;
-			}
+				var = false;
+			} 
+			
 		}
-		if(flag) {
-			System.out.println("검색 결과가 없습니다.\n");
+		if(var) {
+		System.out.println("잘못 입력하셨습니다.\n");
 		}
 	}
 
-	/**
-	 * 학생 정보 검색(일부) 메서드
-	 * 이름에 특정 문자열이 포함되는 학생을 찾아서 조회하는 메서드
-	 */
 	public void searchStudent2() {
-		System.out.println("========= 학생 정보 검색(일부) =========");
-
-		System.out.print("이름에 포함된 문자를 입력하세요 >> ");
-		String input = sc.next();
-
-		boolean flag = true;
+	System.out.println("======= 학생 정보 조회(일부) =======");
 		
+		System.out.print("학생 이름 입력 >> ");
+		String input = sc.next();
+		
+		boolean var = true;
+				
 		for(Student std : studentList) {
-			
-			// boolean String.contains(문자열) : String에 문자열이 포함되면 true 아니면 false 
-			
-			if( std.getName().contains(input)) {
+			if(std.getName().contains(input)) {
 				System.out.println(std + "\n");
-				flag = false;
-			}
+				var = false;
+			} 
+			
 		}
-		if(flag) {
-			System.out.println("검색 결과가 없습니다.\n");
+		if(var) {
+		System.out.println("잘못 입력하셨습니다.\n");
 		}
-	}
 	
+		
+	}
 }
